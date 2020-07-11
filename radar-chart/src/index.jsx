f0e9ee99d13da3280735d9b27745c119009aa9d1
Emily <echen67@gatech.edu>
2020-07-11 13:02:52 -0400

    use arrays instead of individual vars

--- radar-chart/src/index.jsx
+++ radar-chart/src/index.jsx
@@ -129,48 +129,40 @@ const getTotalRecentClosed = async() => {
 const App = () => {
   // Retrieve the configuration
   const config = useConfig();
+  const userConfig = [config.user1, config.user2, config.user3, config.user4, config.user5];
 
   /* GET GLOBAL MAX VALUES */
   // Get total users
   let [totalUsers] = useAction(() => null, async () => await getTotalUsers(""));
   let numUsers = 0;
-  let userName1 = "", userName2 = "", userName3 = "", userName4 = "", userName5 = "";
   let userNames = ["", "", "", "", ""];
   for (var i = 0; i < totalUsers.length; i++) {
     if (totalUsers[i].accountType == "atlassian") { numUsers += 1; }
-    if (totalUsers[i].accountId == config.user1) { userName1 = totalUsers[i].displayName; }
-    if (totalUsers[i].accountId == config.user2) { userName2 = totalUsers[i].displayName; }
-    if (totalUsers[i].accountId == config.user3) { userName3 = totalUsers[i].displayName; }
-    if (totalUsers[i].accountId == config.user4) { userName4 = totalUsers[i].displayName; }
-    if (totalUsers[i].accountId == config.user5) { userName5 = totalUsers[i].displayName; }
+    for (var j = 0; j < userConfig.length; j++) {
+      if (totalUsers[i].accountId == userConfig[j]) { userNames[j] = totalUsers[i].displayName; }
+    }
   }
 
   // Get all recently closed issues
-  let numRecent = 0, userRecent1 = 0, userRecent2 = 0, userRecent3 = 0, userRecent4 = 0, userRecent5 = 0;
+  let numRecent = 0;
+  let userRecents = [0, 0, 0, 0, 0];
   const allRecentIssues = useAction(() => null, async () => await getTotalRecentClosed());
   for (var i = 0; i < allRecentIssues[0].issues.length; i++) {
     numRecent += 1;
     let assignee = allRecentIssues[0].issues[i].fields.assignee;
     if (assignee != null) {
-      if (assignee.accountId == config.user1) { userRecent1 += 1; }
-      else if (assignee.accountId == config.user2) { userRecent2 += 1; }
-      else if (assignee.accountId == config.user3) { userRecent3 += 1; }
-      else if (assignee.accountId == config.user4) { userRecent4 += 1; }
-      else if (assignee.accountId == config.user5) { userRecent5 += 1; }
+      for (var j = 0; j < userRecents.length; j++) {
+        if (assignee.accountId == userConfig[j]) { userRecents[j] += 1; }
+      }
     }
   }
 
   // Get total comments, plus number of comments per user
-  let numComments = 0, userComments1 = 0, userComments2 = 0, userComments3 = 0, userComments4 = 0, userComments5 = 0;
-  let numWatches = 0;
-  let numClosed = 0, userClosed1 = 0, userClosed2 = 0, userClosed3 = 0, userClosed4 = 0, userClosed5 = 0;
-  let numOnTime = 0, userOnTime1 = 0, userOnTime2 = 0, userOnTime3 = 0, userOnTime4 = 0, userOnTime5 = 0;
+  let numComments = 0, numWatches = 0, numClosed = 0, numOnTime = 0;
+  let userComments = [0, 0, 0, 0, 0], userClosed = [0, 0, 0, 0, 0], userOnTime = [0, 0, 0, 0, 0];
   let numLowest = 0, numLow = 0, numMedium = 0, numHigh = 0, numHighest = 0;
-  let userLowest1 = 0, userLow1 = 0, userMedium1 = 0, userHigh1 = 0, userHighest1 = 0;
-  let userLowest2 = 0, userLow2 = 0, userMedium2 = 0, userHigh2 = 0, userHighest2 = 0;
-  let userLowest3 = 0, userLow3 = 0, userMedium3 = 0, userHigh3 = 0, userHighest3 = 0;
-  let userLowest4 = 0, userLow4 = 0, userMedium4 = 0, userHigh4 = 0, userHighest4 = 0;
-  let userLowest5 = 0, userLow5 = 0, userMedium5 = 0, userHigh5 = 0, userHighest5 = 0;
+  let userPriorities = [[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]];
+  const priorities = ["Lowest", "Low", "Medium", "High", "Highest"];
   const allIssues = useAction(() => null, async () => await getTotalIssues());
 
   // Iterate through all issues and then through all comments on each issue
@@ -186,11 +178,9 @@ const App = () => {
     // Get total number of comments and per-user comment data
     numComments += comments.length;
     for (var j = 0; j < comments.length; j++) {
-      if (comments[j].author.accountId == config.user1) { userComments1 += 1; }
-      else if (comments[j].author.accountId == config.user2) { userComments2 += 1; }
-      else if (comments[j].author.accountId == config.user3) { userComments3 += 1; }
-      else if (comments[j].author.accountId == config.user4) { userComments4 += 1; }
-      else if (comments[j].author.accountId == config.user5) { userComments5 += 1; }
+      for (var k = 0; k < userConfig.length; k++) {
+        if (comments[j].author.accountId == userConfig[k]) { userComments[k] += 1; }
+      }
     }
 
     // Also get total number of watches on the way
@@ -205,44 +195,12 @@ const App = () => {
 
     // Get per-user priority data
     if (assignee != null) {
-      if (assignee.accountId == config.user1) {
-        if (priority == "Lowest") { userLowest1 += 1; }
-        else if (priority == "Low") { userLow1 += 1; }
-        else if (priority == "Medium") { userMedium1 += 1; }
-        else if (priority == "High") { userHigh1 += 1; }
-        else if (priority == "Highest") { userHighest1 += 1; }
-      }
-
-      if (assignee.accountId == config.user2) {
-        if (priority == "Lowest") { userLowest2 += 1; }
-        else if (priority == "Low") { userLow2 += 1; }
-        else if (priority == "Medium") { userMedium2 += 1; }
-        else if (priority == "High") { userHigh2 += 1; }
-        else if (priority == "Highest") { userHighest2 += 1; }
-      }
-
-      if (assignee.accountId == config.user3) {
-        if (priority == "Lowest") { userLowest3 += 1; }
-        else if (priority == "Low") { userLow3 += 1; }
-        else if (priority == "Medium") { userMedium3 += 1; }
-        else if (priority == "High") { userHigh3 += 1; }
-        else if (priority == "Highest") { userHighest3 += 1; }
-      }
-
-      if (assignee.accountId == config.user4) {
-        if (priority == "Lowest") { userLowest4 += 1; }
-        else if (priority == "Low") { userLow4 += 1; }
-        else if (priority == "Medium") { userMedium4 += 1; }
-        else if (priority == "High") { userHigh4 += 1; }
-        else if (priority == "Highest") { userHighest4 += 1; }
-      }
-
-      if (assignee.accountId == config.user5) {
-        if (priority == "Lowest") { userLowest5 += 1; }
-        else if (priority == "Low") { userLow5 += 1; }
-        else if (priority == "Medium") { userMedium5 += 1; }
-        else if (priority == "High") { userHigh5 += 1; }
-        else if (priority == "Highest") { userHighest5 += 1; }
+      for (var j = 0; j < userConfig.length; j++) {
+        for (var k = 0; k < priorities.length; k++) {
+          if (assignee.accountId == userConfig[j] && priority == priorities[k]) {
+            userPriorities[j][k] += 1;
+          }
+        }
       }
     }
 
@@ -250,21 +208,17 @@ const App = () => {
     if (status == "Done" || status == "Closed" || status == "Resolved") {
       numClosed += 1;
       if (assignee != null) {
-        if (assignee.accountId == config.user1) { userClosed1 += 1; }
-        else if (assignee.accountId == config.user2) { userClosed2 += 1; }
-        else if (assignee.accountId == config.user3) { userClosed3 += 1; }
-        else if (assignee.accountId == config.user4) { userClosed4 += 1; }
-        else if (assignee.accountId == config.user5) { userClosed5 += 1; }
+        for (var j = 0; j < userConfig.length; j++) {
+          if (assignee.accountId == userConfig[j]) { userClosed[j] += 1; }
+        }
       }
       // resolutiondate != null && duedate >= resolutiondate
       if (duedate == null || duedate >= resolutiondate) {
         numOnTime += 1;
         if (assignee != null) {
-          if (assignee.accountId == config.user1) { userOnTime1 += 1; }
-          else if (assignee.accountId == config.user2) { userOnTime2 += 1; }
-          else if (assignee.accountId == config.user3) { userOnTime3 += 1; }
-          else if (assignee.accountId == config.user4) { userOnTime4 += 1; }
-          else if (assignee.accountId == config.user5) { userOnTime5 += 1; }
+          for (var j = 0; j < userConfig.length; j++) {
+            if (assignee.accountId == userConfig[j]) { userOnTime[j] += 1; }
+          }
         }
       }
     }
@@ -273,19 +227,19 @@ const App = () => {
   // Transform to chart data
   let [userWatchedIssues1] = useAction(() => null, async () => await getWatchedIssues(config.user1));
   let [userWatchedIssues2] = useAction(() => null, async () => await getWatchedIssues(config.user2));
-  const userP1 = userLowest1 + 2*userLow1 + 3*userMedium1 + 4*userHigh1 + 5*userHighest1;
-  const userP2 = userLowest2 + 2*userLow2 + 3*userMedium2 + 4*userHigh2 + 5*userHighest2;
+  const userP1 = userPriorities[0][0] + 2*userPriorities[0][1] + 3*userPriorities[0][2] + 4*userPriorities[0][3] + 5*userPriorities[0][4];
+  const userP2 = userPriorities[1][0] + 2*userPriorities[1][1] + 3*userPriorities[1][2] + 4*userPriorities[1][3] + 5*userPriorities[1][4];
   const avgCommunication = numComments / numUsers;
   const avgInvolvement = numWatches / numUsers;
   const avgPriority = (numLowest + 2*numLow + 3*numMedium + 4*numHigh + 5*numHighest) / numUsers;
   const avgDeadlines = numOnTime / numClosed;
   const avgProductivity = numRecent / numUsers;
 
-  let communicationUser1 = (avgCommunication != 0) ? (userComments1 / avgCommunication) : 0;
+  let communicationUser1 = (avgCommunication != 0) ? (userComments[0] / avgCommunication) : 0;
   let involvementUser1 = (avgInvolvement != 0) ? (userWatchedIssues1.issues.length / avgInvolvement) : 0;
   let technicalUser1 = (avgPriority != 0) ? (userP1 / avgPriority) : 0;
-  let deadlinesUser1 = (avgDeadlines != 0) ? ((userOnTime1 / userClosed1) / avgDeadlines) : 0;
-  let productivityUser1 = (avgProductivity != 0) ? (userRecent1 / avgProductivity) : 0;
+  let deadlinesUser1 = (avgDeadlines != 0) ? ((userOnTime[0] / userClosed[0]) / avgDeadlines) : 0;
+  let productivityUser1 = (avgProductivity != 0) ? (userRecents[0] / avgProductivity) : 0;
   const max1 = Math.max(communicationUser1, involvementUser1, technicalUser1, deadlinesUser1, productivityUser1);
   if (max1 != 0) {
     communicationUser1 /= max1;
@@ -295,11 +249,11 @@ const App = () => {
     productivityUser1 /= max1;
   }
 
-  let communicationUser2 = (avgCommunication != 0) ? (userComments2 / avgCommunication) : 0;
+  let communicationUser2 = (avgCommunication != 0) ? (userComments[1] / avgCommunication) : 0;
   let involvementUser2 = (avgInvolvement != 0) ? (userWatchedIssues2.issues.length / avgInvolvement) : 0;
   let technicalUser2 = (avgPriority != 0) ? (userP2 / avgPriority) : 0;
-  let deadlinesUser2 = (avgDeadlines != 0) ? ((userOnTime2 / userClosed2) / avgDeadlines) : 0;
-  let productivityUser2 = (avgProductivity != 0) ? (userRecent2 / avgProductivity) : 0;
+  let deadlinesUser2 = (avgDeadlines != 0) ? ((userOnTime[1] / userClosed[1]) / avgDeadlines) : 0;
+  let productivityUser2 = (avgProductivity != 0) ? (userRecents[1] / avgProductivity) : 0;
   const max2 = Math.max(communicationUser2, involvementUser2, technicalUser2, deadlinesUser2, productivityUser2);
   if (max2 != 0) {
     communicationUser2 /= max2;
@@ -310,29 +264,29 @@ const App = () => {
   }
 
   // Transform to text (remove in final version)
-  userComments1 = `Comments: ${userComments1}, Communication: ${userComments1 / avgCommunication}`;
-  userComments2 = `Comments: ${userComments2}, Communication: ${userComments2 / avgCommunication}`;
+  const userComments1 = `Comments: ${userComments[0]}, Communication: ${userComments[0] / avgCommunication}`;
+  const userComments2 = `Comments: ${userComments[1]}, Communication: ${userComments[1] / avgCommunication}`;
   numComments = `Total comments: ${numComments}, Average comments: ${avgCommunication}`;
 
   userWatchedIssues1 = `Watches: ${userWatchedIssues1.issues.length}, Involvement: ${userWatchedIssues1.issues.length / avgInvolvement}`;
   userWatchedIssues2 = `Watches: ${userWatchedIssues2.issues.length}, Involvement: ${userWatchedIssues2.issues.length / avgInvolvement}`;
   numWatches = `Total watches: ${numWatches}, Average watches: ${avgInvolvement}`;
 
-  const userPriority1 = `Priority: [${userLowest1}, ${userLow1}, ${userMedium1}, ${userHigh1}, ${userHighest1}]: ${userP1}, Technical: ${userP1 / avgPriority}`;
-  const userPriority2 = `Priority: [${userLowest2}, ${userLow2}, ${userMedium2}, ${userHigh2}, ${userHighest2}]: ${userP2}, Technical: ${userP2 / avgPriority}`;
+  const userPriority1 = `Priority: [${userPriorities[0]}]: ${userP1}, Technical: ${userP1 / avgPriority}`;
+  const userPriority2 = `Priority: [${userPriorities[1]}]: ${userP2}, Technical: ${userP2 / avgPriority}`;
   const totalPriority = `Total Priority: ${numLowest}, ${numLow}, ${numMedium}, ${numHigh}, ${numHighest}, Average Priority: ${avgPriority}`;
 
-  userClosed1 = `Closed issues: ${userClosed1}, On-time issues: ${userOnTime1}, Met Deadlines: ${userOnTime1 / userClosed1}, Deadlines: ${(userOnTime1 / userClosed1) / avgDeadlines}`;
-  userClosed2 = `Closed issues: ${userClosed2}, On-time issues: ${userOnTime2}, Met Deadlines: ${userOnTime2 / userClosed2}, Deadlines: ${(userOnTime2 / userClosed2) / avgDeadlines}`;
+  const userClosed1 = `Closed issues: ${userClosed[0]}, On-time issues: ${userOnTime[0]}, Met Deadlines: ${userOnTime[0] / userClosed[0]}, Deadlines: ${(userOnTime[0] / userClosed[0]) / avgDeadlines}`;
+  const userClosed2 = `Closed issues: ${userClosed[1]}, On-time issues: ${userOnTime[1]}, Met Deadlines: ${userOnTime[1] / userClosed[1]}, Deadlines: ${(userOnTime[1] / userClosed[1]) / avgDeadlines}`;
   numClosed = `Total closed issues: ${numClosed}, Total on-time issues: ${numOnTime}, Average On-Time: ${avgDeadlines}`;
 
-  userRecent1 = `Recently Closed: ${userRecent1}, Productivity: ${userRecent1 / avgProductivity}`;
-  userRecent2 = `Recently Closed: ${userRecent2}, Productivity: ${userRecent2 / avgProductivity}`;
+  const userRecent1 = `Recently Closed: ${userRecents[0]}, Productivity: ${userRecents[0] / avgProductivity}`;
+  const userRecent2 = `Recently Closed: ${userRecents[1]}, Productivity: ${userRecents[1] / avgProductivity}`;
   numRecent = `Total Recent: ${numRecent}, Average Recent: ${avgProductivity}`;
 
   numUsers = `Total users: ${numUsers}`;
-  userName1 = `User 1: ${userName1}`;
-  userName2 = `User 2: ${userName2}`;
+  const userName1 = `User 1: ${userNames[0]}`;
+  const userName2 = `User 2: ${userNames[1]}`;
 
 
   /* ------------------------- DRAW RADAR CHART ------------------------- */
